5ce4b2e370d9944d33056ae644c853d7c7ef151e
intsys517@gmail.com <intsys517@gmail.com>
2025-03-07 22:53:24 +0000

    Premier commit

--- src/main/java/com/resto/demo/config/SecurityConfig.java
+++ src/main/java/com/resto/demo/config/SecurityConfig.java
@@ -6,7 +6,9 @@ package com.resto.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,33 +21,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @SuppressWarnings({ "deprecation", "removal" })
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/authenticate").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .exceptionHandling(withDefaults())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                .authorizeRequests(requests -> {
+                    try {
+                        requests
+                        .requestMatchers("/authenticate").permitAll()
+                        .requestMatchers("/api/public/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                        .and()
+                        .exceptionHandling(withDefaults()).sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                });
 
         // Ajoutez le filtre JWT ici
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+        
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        
+            return http.build();
+        }
 
-    @Bean
-    public JwtRequestFilter jwtAuthenticationFilter() throws Exception {
-        return new JwtRequestFilter();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+
+        @Bean
+        public JwtRequestFilter jwtAuthenticationFilter() throws Exception {
+            return new JwtRequestFilter();
+        }
 }
--- src/main/java/com/resto/demo/controller/AuthenticationController.java
+++ src/main/java/com/resto/demo/controller/AuthenticationController.java
@@ -1,7 +1,5 @@
 package com.resto.demo.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
